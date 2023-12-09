edaecd6c7d1fa7f08a77cb1e7599c6e33cfc8585
connor-lynchh <cowlynch@ucsc.edu>
2023-12-09 14:29:27 -0800

    hellow world

--- src/Overworld.js
+++ src/Overworld.js
@@ -3,11 +3,17 @@ class Example extends Phaser.Scene
     propertiesText;
     marker;
     map;
+    light;
+    player;
+    radius = 35;
+    themeMusic;
 
     preload ()//"100 and 99 are both pink"
     {
         // this.load.tilemapTiledJSON('map', 'assets/tilemaps/maps/tile_properties-v12.json');
+       
         //this.load.tilemapTiledJSON('map', 'assets/tile_properties.json');
+        this.load.audio('theme', 'assets/Myst_theme.mp3');
         var coin = Math.floor(Math.random() * 2);
         if(coin==1){
             this.load.tilemapTiledJSON('map', 'assets/tile_properties2.json');
@@ -19,14 +25,14 @@ class Example extends Phaser.Scene
 
         //this.load.image('tiles', 'assets/gridtiles2.png');
         this.load.image('tiles', 'assets/gridtiles.png');
-        this.load.image('car', 'assets/car90.png');
+        this.load.image('player', 'assets/player.png');
     }
 
     create ()
     {
         this.map = this.make.tilemap({ key: 'map' });
         const tileset = this.map.addTilesetImage('tiles');
-        const layer = this.map.createLayer('Tile Layer 1', tileset, 0, 0);
+        const layer = this.map.createLayer('Tile Layer 1', tileset, 0, 0).setPipeline('Light2D');
         this.propertiesText = this.add.text(16, 540, '', {
             fontSize: '18px',
             fill: '#ffffff'
@@ -41,21 +47,31 @@ class Example extends Phaser.Scene
             fill: '#ffffff'
         });
         help.setScrollFactor(0);
-        const player = this.add.image(32 + 16, 32 + 16, 'car');
+        this.player = this.add.image(32 + 16, 32 + 16, 'player');
+
+        this.light = this.lights.addLight(0,0,this.radius,0xffffff,5.0).setScrollFactor(0.0);
+        this.lights.enable().setAmbientColor(0x000000);
         
+        const music = this.sound.add('theme');
+        music.play();
+        music.volume = 0.25;
+
+        this.sound.pauseOnBlur = true;
         
         this.input.keyboard.on('keydown-A', event =>
         {
 
-            const tile = layer.getTileAtWorldXY(player.x - 32, player.y, true);
+            const tile = layer.getTileAtWorldXY(this.player.x - 32, this.player.y, true);
 
             if (tile.index === 1 || tile.index === 29 || tile.index ===99)
             {
                 //  Blocked, we can't move
-                player.x -= 32;
+                this.player.x -= 32;
             }else if(tile.index===73){
-                player.x=48
-                player.y=48
+                this.player.x=48
+                this.player.y=48
+                //this.light = this.lights.addLight(0,0,this.radius+=this.radius,0xffffff,5.0).setScrollFactor(0.0); // increases light radius for when hitting a trap
+                this.light.radius += this.light.radius;
             }else if(tile.index===100){
                 var rect = this.add.rectangle(0, 0, 10000, 10000, "Black", 1);
                 this.add.text(150, 256, 'You escaped the maze and won! ', { fontSize: '30px' });
@@ -67,15 +83,17 @@ class Example extends Phaser.Scene
         this.input.keyboard.on('keydown-D', event =>
         {
 
-            const tile = layer.getTileAtWorldXY(player.x + 32, player.y, true);
+            const tile = layer.getTileAtWorldXY(this.player.x + 32, this.player.y, true);
 
             if (tile.index === 1 || tile.index === 29 || tile.index === 99 || tile.index ===100)
             {
                 //  Blocked, we can't move
-                player.x += 32;
+                this.player.x += 32;
             }else if(tile.index===73){
-                player.x=48
-                player.y=48
+                this.player.x=48
+                this.player.y=48
+                //this.light = this.lights.addLight(0,0,this.radius+=this.radius,0xffffff,5.0).setScrollFactor(0.0); // increases light radius for when hitting a trap
+                this.light.radius += this.light.radius;
             }
 
 
@@ -85,15 +103,17 @@ class Example extends Phaser.Scene
         this.input.keyboard.on('keydown-W', event =>
         {
 
-            const tile = layer.getTileAtWorldXY(player.x, player.y - 32, true);
+            const tile = layer.getTileAtWorldXY(this.player.x, this.player.y - 32, true);
 
             if (tile.index === 1 || tile.index === 29 || tile.index === 99 || tile.index ===100)
             {
                 //  Blocked, we can't move
-                player.y -= 32;
+                this.player.y -= 32;
             }else if(tile.index===73){
-                player.x=48
-                player.y=48
+                this.player.x=48
+                this.player.y=48
+                // this.light = this.lights.addLight(0,0,this.radius+=this.radius,0xffffff,5.0).setScrollFactor(0.0); // increases light radius for when hitting a trap
+                this.light.radius += this.light.radius;
             }
 
         });
@@ -102,15 +122,17 @@ class Example extends Phaser.Scene
         this.input.keyboard.on('keydown-S', event =>
         {
 
-            const tile = layer.getTileAtWorldXY(player.x, player.y + 32, true);
+            const tile = layer.getTileAtWorldXY(this.player.x, this.player.y + 32, true);
 
             if (tile.index === 1 || tile.index === 29 || tile.index === 99 || tile.index ===100)
             {
                 //  Blocked, we can't move
-                player.y += 32;
+                this.player.y += 32;
             }else if(tile.index===73){
-                player.x=48
-                player.y=48
+                this.player.x=48
+                this.player.y=48
+                //this.light = this.lights.addLight(0,0,this.radius+=5,0xffffff,5.0).setScrollFactor(0.0); // increases light radius for when hitting a trap
+                this.light.radius += this.light.radius;
             }
 
         });
@@ -146,7 +168,8 @@ class Example extends Phaser.Scene
                 tile.properties.viewed = true;
             }
         }*/
-        
+        this.light.x = this.player.x;
+        this.light.y = this.player.y;
     }
 }
 function formatTime(seconds){
@@ -167,7 +190,8 @@ const config = {
     backgroundColor: '#000000',
     parent: 'phaser-example',
     pixelArt: true,
-    scene: Example
+    scene: Example,
+   
 };
 
 const game = new Phaser.Game(config);
